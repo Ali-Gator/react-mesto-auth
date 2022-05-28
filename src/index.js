a327e5401fe223b408a9185a57dc6df4a6510aec
Oleg Gordienov <ogordienov@yandex.ru>
2022-05-28 17:39:08 +0200

    fix: add leading slash to basename

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-      <BrowserRouter basename="react-mesto-auth">
-        <App/>
-      </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/react-mesto-auth">
+      <App/>
+    </BrowserRouter>
+  </React.StrictMode>
 );
